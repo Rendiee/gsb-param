4085e3453c046fa934c06788382967d67dac0ef0
ScientistsVSAliens <metabearsnft@protonmail.com>
2022-03-30 23:52:56 +0200

    start product card

--- vues/v_produits.php
+++ vues/v_produits.php
@@ -4,23 +4,32 @@
 			<div class="col-3">
 				<h1>Test</h1>
 			</div>
-			<div class="col-9 d-flex">
+			<div class="col-9 d-flex flex-column flex-wrap">
+				<div class="row row-cols-3">
 				<?php
 				
-					foreach( $lesProduits as $unProduit) 
-					{ 
-						$description = $unProduit['description'];
-						$prix = $unProduit['prix'];
-						$image = "assets/" . $unProduit['photo'];
-						$nom = $unProduit['nom'];
-						$marque = $unProduit['marque'];
+				foreach( $lesProduits as $unProduit) 
+				{ 
+					$description = $unProduit['description'];
+					$prix = $unProduit['prix'];
+					$image = "assets/" . $unProduit['photo'];
+					$nom = $unProduit['nom'];
+					$marque = $unProduit['marque'];
+
+					$description = substr($description, 0, 116);
+					$description = $description . '...';
 
 				?>
 
-				<div class="card-product">
-					<p class="marque-product"><?php echo $marque ?></p>
-					<p><?php echo $nom ?></p>
-					<img src="<?php echo $image ?>" alt="image product" />
+				<div class="col">
+					<div class="card-product">
+						<p class="marque-product"><?php echo $marque ?></p>
+						<div class="title-height">
+							<p class="name-product"><?php echo $nom ?></p>
+						</div>
+						<img class="img-product" src="<?php echo $image ?>" alt="image product" />
+						<p class="description-product"><?php echo $description ?></p>
+					</div>
 				</div>
 
 				<?php
@@ -28,6 +37,7 @@
 					}
 
 				?>
+				</div>
 			</div>
 		</div>
 	</div>
