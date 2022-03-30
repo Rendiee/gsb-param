db280021cfc59e51a428e769f0d254139e67c266
TristanDs <d.s.tristan@outlook.fr>
2022-03-30 19:53:18 +0200

    fix sql panier + test css

--- vues/v_produits.php
+++ vues/v_produits.php
@@ -6,26 +6,25 @@
 			</div>
 			<div class="col-9 d-flex">
 				<?php
-				
-					foreach( $lesProduits as $unProduit) 
-					{ 
-						$description = $unProduit['description'];
-						$prix = $unProduit['prix'];
-						$image = "assets/" . $unProduit['photo'];
-						$nom = $unProduit['nom'];
-						$marque = $unProduit['marque'];
+
+				foreach ($lesProduits as $unProduit) {
+					$description = $unProduit['description'];
+					$prix = $unProduit['prix'];
+					$image = "assets/" . $unProduit['photo'];
+					$nom = $unProduit['nom'];
+					$marque = $unProduit['marque'];
 
 				?>
 
-				<div class="card-product">
-					<p class="marque-product"><?php echo $marque ?></p>
-					<p><?php echo $nom ?></p>
-					<img src="<?php echo $image ?>" alt="image product" />
-				</div>
+					<div class="card-product">
+						<p class="marque-product"><?php echo $marque ?></p>
+						<p><?php echo $nom ?></p>
+						<img src="<?php echo $image ?>" alt="image product" />
+					</div>
 
 				<?php
-				
-					}
+
+				}
 
 				?>
 			</div>
@@ -34,32 +33,33 @@
 </div>
 
 <div class="w-100 text-center h2 text-success fw-bold">Tous les produits</div>
-<div id="produits" class="d-flex flex-wrap justify-content-around col-lg-8 col-12 m-auto">
+<div id="produits" class="d-flex flex-wrap justify-content-around col-xxl-8 col-lg-10 col-9 m-auto">
 	<?php
 	// parcours du tableau contenant les produits à afficher
-	if(isset($message)){echo '<div class="alert alert-danger mt-3">'.$message.'</div>';}
-	foreach( $lesProduits as $unProduit) 
-	{ 	// récupération des informations du produit
+	if (isset($message)) {
+		echo '<div class="alert alert-danger mt-3">' . $message . '</div>';
+	}
+	foreach ($lesProduits as $unProduit) { 	// récupération des informations du produit
 		$id = $unProduit['id'];
 		$description = $unProduit['description'];
-		$prix=$unProduit['prix'];
+		$prix = $unProduit['prix'];
 		$image = "assets/" . $unProduit['photo'];
 		$nom = $unProduit['nom'];
 		// affichage d'un produit avec ses informations
-		?>	
+	?>
 		<div class="card d-flex flex-column justify-content-between">
 			<div class="d-flex flex-column justify-content-center align-items-center">
 				<div class="photoCard"><img src="<?php echo $image ?>" alt=image /></div>
 				<div class="descrCard"><?php echo $nom ?></div>
 			</div>
 			<div class="d-flex flex-wrap justify-content-around align-items-center">
-				<div class=""><?php echo $prix."€" ?></div>		
+				<div class=""><?php echo $prix . "€" ?></div>
 				<a href="index.php?uc=voirProduits&produit=<?php echo $id ?>&action=ajouterAuPanier">
 					<img src="assets/images/mettrepanier.png" TITLE="Ajouter au panier" alt="Mettre au panier">
 				</a>
 			</div>
 		</div>
-	<?php			
+	<?php
 	} // fin du foreach qui parcourt les produits
 	?>
 </div>
--- vues/v_produitsDeCategorie.php
+++ vues/v_produitsDeCategorie.php
@@ -1,17 +1,16 @@
 <div class="w-100 text-center h2 text-success fw-bold">
 	<?php
-	
+
 	?>
 </div>
 <div class="d-flex flex-wrap justify-content-center">
 	<div id="categories" class="list-group my-2">
 		<?php
-		foreach( $lesCategories as $uneCategorie) 
-		{
+		foreach ($lesCategories as $uneCategorie) {
 			$idCategorie = $uneCategorie['ca_acronyme'];
 			$libCategorie = $uneCategorie['ca_libelle'];
-			?>
-				<a id="categorie" class="list-group-item list-group-item-action" href="index.php?uc=voirProduits&categorie=<?php echo $idCategorie ?>&action=voirProduits">
+		?>
+			<a id="categorie" class="list-group-item list-group-item-action" href="index.php?uc=voirProduits&categorie=<?php echo $idCategorie ?>&action=voirProduits">
 				<?php echo $libCategorie ?></a>
 		<?php
 		}
@@ -21,27 +20,26 @@
 		<?php
 		// parcours du tableau contenant les produits à afficher
 
-		foreach( $lesProduits as $unProduit) 
-		{ 	// récupération des informations du produit
+		foreach ($lesProduits as $unProduit) { 	// récupération des informations du produit
 			$id = $unProduit['id'];
 			$description = $unProduit['nom'];
-			$prix=$unProduit['prix'];
+			$prix = $unProduit['prix'];
 			$image = "assets/" . $unProduit['photo'];
 			// affichage d'un produit avec ses informations
-			?>	
+		?>
 			<div class="card mt-2 d-flex flex-column justify-content-between">
 				<div class="d-flex flex-column justify-content-center align-items-center">
 					<div class="photoCard"><img src="<?php echo $image ?>" alt=image /></div>
 					<div class="descrCard"><?php echo $description ?></div>
 				</div>
 				<div class="d-flex flex-wrap justify-content-around align-items-center">
-					<div class=""><?php echo $prix."€" ?></div>		
+					<div class=""><?php echo $prix . "€" ?></div>
 					<a id="categProduit" href="index.php?uc=voirProduits&categorie=<?php echo $categorie ?>&produit=<?php echo $id ?>&action=ajouterAuPanier">
 						<img src="assets/images/mettrepanier.png" TITLE="Ajouter au panier" alt="Mettre au panier">
 					</a>
 				</div>
 			</div>
-		<?php			
+		<?php
 		} // fin du foreach qui parcourt les produits
 		?>
 	</div>
