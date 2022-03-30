3f4dac84a8cecb6bd3fbcb6c9671a0b873359555
ScientistsVSAliens <metabearsnft@protonmail.com>
2022-03-30 23:56:28 +0200

    fix conflict

--- vues/v_produits.php
+++ vues/v_produits.php
@@ -1,4 +1,5 @@
 <div class="container-fluid">
+	<div class="w-100 text-center h2 text-success fw-bold">Tous les produits</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-3">
@@ -7,7 +8,9 @@
 			<div class="col-9 d-flex flex-column flex-wrap">
 				<div class="row row-cols-3">
 				<?php
-				
+				if (isset($message)) {
+					echo '<div class="alert alert-danger mt-3">' . $message . '</div>';
+				}
 				foreach( $lesProduits as $unProduit) 
 				{ 
 					$description = $unProduit['description'];
@@ -41,36 +44,4 @@
 			</div>
 		</div>
 	</div>
-</div>
-
-<div class="w-100 text-center h2 text-success fw-bold">Tous les produits</div>
-<div id="produits" class="d-flex flex-wrap justify-content-around col-xxl-8 col-lg-10 col-9 m-auto">
-	<?php
-	// parcours du tableau contenant les produits à afficher
-	if (isset($message)) {
-		echo '<div class="alert alert-danger mt-3">' . $message . '</div>';
-	}
-	foreach ($lesProduits as $unProduit) { 	// récupération des informations du produit
-		$id = $unProduit['id'];
-		$description = $unProduit['description'];
-		$prix = $unProduit['prix'];
-		$image = "assets/" . $unProduit['photo'];
-		$nom = $unProduit['nom'];
-		// affichage d'un produit avec ses informations
-	?>
-		<div class="card d-flex flex-column justify-content-between">
-			<div class="d-flex flex-column justify-content-center align-items-center">
-				<div class="photoCard"><img src="<?php echo $image ?>" alt=image /></div>
-				<div class="descrCard"><?php echo $nom ?></div>
-			</div>
-			<div class="d-flex flex-wrap justify-content-around align-items-center">
-				<div class=""><?php echo $prix . "€" ?></div>
-				<a href="index.php?uc=voirProduits&produit=<?php echo $id ?>&action=ajouterAuPanier">
-					<img src="assets/images/mettrepanier.png" TITLE="Ajouter au panier" alt="Mettre au panier">
-				</a>
-			</div>
-		</div>
-	<?php
-	} // fin du foreach qui parcourt les produits
-	?>
 </div>
